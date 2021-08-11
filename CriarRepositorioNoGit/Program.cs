33b66a52c57f6fd8b6636ed1e6ecc4a0e4d1f908
Roni Torres <ronitls1989@gmail.com>
2021-08-11 12:37:40 -0300

    Adicionei a segunda linha de texto

---
+++ CriarRepositorioNoGit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CriarRepositorioNoGit {
+    class Program {
+        static void Main(string[] args) {
+
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Aprendendo a gerar versoes");
+
+
+        }
+    }
+}
